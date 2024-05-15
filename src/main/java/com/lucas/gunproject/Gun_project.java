5632927904f9944ed6c0a2f07bec0f0e44d34941
Lucas Cividini <lucaspagnussat5@gmail.com>
2024-05-15 15:31:58 -0300

    Initial commit

---
+++ src/main/java/com/lucas/gunproject/Gun_project.java
@@ -0,0 +1,17 @@
+package com.lucas.gunproject;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Gun_project extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
