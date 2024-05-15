ae62d20fffc24be985a92be96b22446c2d4315c4
Lucas Cividini <lucaspagnussat5@gmail.com>
2024-05-15 15:50:25 -0300

    final commit

--- src/main/java/com/lucas/gunproject/Gun_project.java
+++ src/main/java/com/lucas/gunproject/Gun_project.java
@@ -1,17 +1,38 @@
 package com.lucas.gunproject;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Egg;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Snowball;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Gun_project extends JavaPlugin {
+public final class Gun_project extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
 
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e) {
+
+        if(e.hasItem()){
+            Player player = e.getPlayer();
+            if(e.getItem().getType().equals(Material.DIAMOND_HOE)){
+                player.launchProjectile(Snowball.class);
+
+
+            } else if(e.getItem().getType().equals(Material.IRON_HOE)){
+                player.launchProjectile(Egg.class);
+            }
+        }
     }
 }
